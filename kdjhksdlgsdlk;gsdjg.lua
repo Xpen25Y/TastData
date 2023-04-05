25c632e05338732bc990e63603681f048a26e6ad
Xpen25Y <122020825+Xpen25Y@users.noreply.github.com>
2023-04-05 22:25:13 +0700

    Create kdjhksdlgsdlk;gsdjg.lua

---
+++ kdjhksdlgsdlk;gsdjg.lua
@@ -0,0 +1,40 @@
+local HWID = nil
+local HWIDPlr = game:GetService("RbxAnalyticsService"):GetClientId()
+local key = _G.Key
+local check = "ลิ้งค์check.php" .. key
+local KeyAndHWID = false
+local ScriptIsRunning = false
+--------------------------------------------------------------------------------------------------------
+function Whitelist1()
+    if game:HttpGet(check) == "Whitelisted" then
+    HWID = nil
+        else
+            game.Players.LocalPlayer:kick("Invaid Key")
+    end
+
+    if HWID == HWIDPlr then
+        KeyAndHWID = true
+    else
+        setclipboard(game:GetService("RbxAnalyticsService"):GetClientId())
+        wait()
+        game.Players.LocalPlayer:kick("Invaid HWID")
+    end
+end
+
+
+
+
+if _G.Key == "Data-H2A9-HGSA-45GA" then
+    Whitelist1()
+end
+
+if KeyAndHWID == true then
+    ScriptIsRunning = true
+end
+
+if ScriptIsRunning == true then
+    loadstring(game:HttpGet("UR Script"))()
+    return
+end
+
+
